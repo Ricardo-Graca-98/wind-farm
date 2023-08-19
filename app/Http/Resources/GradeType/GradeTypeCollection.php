6b24b5a10d06e2f5122764d7c1cf9c9acb4e1584
Ricardo <ricardojg1@hotmail.com>
2023-08-19 18:47:14 +0100

    feat: add grade type resource/collection

---
+++ app/Http/Resources/GradeType/GradeTypeCollection.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Resources\GradeType;
+
+use Illuminate\Http\Resources\Json\ResourceCollection;
+
+class GradeTypeCollection extends ResourceCollection
+{
+    /**
+     * Transform the resource collection into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return parent::toArray($request);
+    }
+}
